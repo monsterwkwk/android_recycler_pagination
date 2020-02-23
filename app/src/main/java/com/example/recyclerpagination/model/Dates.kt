cd9356fd208f2f2aebfd103dca2aac12559df9e7
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2020-02-24 04:56:14 +0700

    implement parcelable and parcelize in model

--- app/src/main/java/com/example/recyclerpagination/model/Dates.kt
+++ app/src/main/java/com/example/recyclerpagination/model/Dates.kt
@@ -1,6 +1,10 @@
 package com.example.recyclerpagination.model
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class Dates(
     val maximum: String,
     val minimum: String
-)
+): Parcelable
--- app/src/main/java/com/example/recyclerpagination/model/TheMovieDb.kt
+++ app/src/main/java/com/example/recyclerpagination/model/TheMovieDb.kt
@@ -1,9 +1,13 @@
 package com.example.recyclerpagination.model
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class TheMovieDb(
     val dates: Dates,
     val page: Int,
     val results: List<Result>,
     val total_pages: Int,
     val total_results: Int
-)
+):Parcelable
